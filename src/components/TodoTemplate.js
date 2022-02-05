992b16b78f81318406ffcf968eb180dfb20a7678
iamoki <76725512+iamoki@users.noreply.github.com>
2022-02-06 00:04:49 +0900

    컴포넌트 스타일링

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -4,8 +4,8 @@ import './TodoTemplate.scss';
 const TodoTemplate = ({ children }) => {
     return (
         <div className="TodoTemplate">
-             <div className="app-title">일정 관리</div>
-             <div className="content">{children}</div>
+            <div className="app-title">일정 관리</div>
+            <div className="content">{children}</div>
         </div>
     )
 }
