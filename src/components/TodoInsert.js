ecbb0a004703a373727a314fb5c27e8e693e47b1
iamoki <76725512+iamoki@users.noreply.github.com>
2022-02-06 19:19:13 +0900

    투두리스트 초기값세팅, 체크clsassnames로 상태에따라 나타내기, insert값 받아와서 할일 추가하기

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+
+            e.preventDefault();
+        },
+        [onInsert, value],
+    )
+
     return (
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요"/>
+        // 버튼에 온클릭 이벤트를 준것이 아닌 폼자체에 onSubmit이벤트를 설정한 이유:
+        // 해당방식으로 걸면 엔터를 눌러도 이벤트가 발생한다 온클릭,온키프레스 두번 함수를 걸지 않아도됨
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input 
+                placeholder="할 일을 입력하세요"
+                value={value}
+                onChange={onChange}
+            />
             <button type="submit">
                 <MdAdd />
             </button>
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -4,20 +4,24 @@ import {
     MdCheckBox,
     MdRemoveCircleOutline,
 }   from 'react-icons/md';
+import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = () => {
+const TodoListItem = ({todo}) => {
+    const { text, checked } = todo;
+
     return (
         <div className="TodoListItem">
-            <div className="checkbox">
-                <MdCheckBoxOutlineBlank />
-                <div className="text">할 일</div>
+            {/* checkbox는 기본값이며 checked는 true일 때만 나타난다. */}
+            <div className={cn('checkbox', {checked})}>
+                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                <div className="text">{text}</div>
             </div>
             <div className="remove">
                 <MdRemoveCircleOutline />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default TodoListItem;
